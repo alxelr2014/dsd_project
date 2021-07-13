00f20a1d9ee7a867173aafc0f6df049eb273b418
MJavad Hezareh <55049080+MJavadHzr@users.noreply.github.com>
2021-07-13 19:36:02 +0430

    main-cu updated | direct memory use

--- Coprocessor/main_control_unit.v
+++ Coprocessor/main_control_unit.v
@@ -1,41 +1,32 @@
 `timescale 1ns/1ns
 
 module main_CU #(
-	parameter p = 4,
-	parameter index_width = 8,
-	parameter greek_size = 8,
-	parameter memory_size = 1024,
-	parameter memory_size_log = 10
+	parameter p = 4,				//number of processors
+	parameter index_width = 8,		//width of i, j, lambda, gamma, mu
+	parameter memory_size = 1024,	//:)
+	parameter memory_size_log = 10	//:)
 ) (
-    input i_Data_Ready,	// status in memory 
-    input i_Grant,		// show to having grant
+    input [31:0] i_Config,	// config in memory
+	input [31:0] i_Status, // Status in memory 
     input i_Clock,
     input i_Indexes_Received,
     input i_Result_Ready,
 	input i_Reset,
-
-    inout[31:0] io_Memory_Data,
-
-    output reg[31:0] o_Config,
-    output reg o_Grant_Request,
-    output reg[memory_size_log-1:0] o_Memory_Address,
     output wire[index_width-1:0] o_Row_Index,
     output wire[index_width-1:0] o_Column_Index,
     output reg[p-1:0] o_Indexes_Ready,
-	output reg o_Write_Enable
+	output [31:0] o_Status,
+	output reg o_Write_Status_Enable
 );
 
 //counters
 reg[$clog2(p):0] r_Processor_Counter;	// 0 to p
-reg[2*greek_size:0] r_Scatter_Counter;	// 0 to \theta
-reg r_Status_Counter;	// used to count to frist read status and then update it!
-reg r_Read_Counter;		// useed to count one clock to read!
+reg[2*index_width:0] r_Scatter_Counter;	// 0 to \theta
 
 //[scattering limit] and [matrix dimensions] paramter
-reg[greek_size-1:0] r_Theta;
-reg[greek_size-1:0] r_Gamma;
-reg[greek_size-1:0] r_Lambda;
-reg[greek_size-1:0] r_mu;
+reg[index_width-1:0] r_Theta;
+reg[index_width-1:0] r_Gamma;
+reg[index_width-1:0] r_Lambda;
 
 //scattering registers
 reg[index_width-1:0] r_row;
@@ -44,188 +35,104 @@ reg[index_width-1:0] r_column;
 assign o_Row_Index = r_row;
 assign o_Column_Index = r_column;
 
-//bidirectional port
-wire[31:0] r_Data_In;	// r_Memory_Write = 0
-reg[31:0] r_Data_Out;	// r_Memory_Write = 1
-reg r_Memory_Write;		// 1: write, 0: read
-
-assign io_Memory_Data = (r_Memory_Write) ? r_Data_Out : 'bz;
-assign r_Data_In = io_Memory_Data;
-
-// reg [31:0] r_Config;
-// assign o_Config = r_Config;
+//to update status
+reg [31:0] r_Status;
+assign o_Status = r_Status;
 
 //states
 reg[2:0] r_State;
 
 localparam s_Idle = 3'b000 ;
-localparam s_Request_Config_Grant = 3'b001;
-localparam s_Read_Config = 3'b010;
-localparam s_Scatter = 3'b011; // پخش کردن بلوک‌ها بین پردازنده‌ها
-localparam s_Wait_For_Ready = 3'b100;
-localparam s_Request_Status_Grant = 3'b101;
-localparam s_Change_Status = 3'b110;
+localparam s_Read_Config = 3'b01;
+localparam s_Scatter = 3'b010; // پخش کردن بلوک‌ها بین پردازنده‌ها
+localparam s_Wait_For_Ready = 3'b011;
+localparam s_Change_Status = 3'b100;
 
 always @(posedge i_Clock or negedge i_Reset) begin
 	if (!i_Reset) begin
 		r_State <= 0;
-		r_Data_Out <= 0;
-		r_Memory_Write <= 0;
-		// r_Config <= 0;
-		o_Config <= 0;
 		r_row <= 0;
 		r_column <= 0;
 		r_Processor_Counter <= 0;
 		r_Scatter_Counter <= 0;
-		r_Status_Counter <= 0;
-		r_Read_Counter <= 0;
 		r_Theta <= 0;
 		r_Gamma <= 0;
 		r_Lambda <= 0;
-		r_mu <= 0;
-		o_Grant_Request <= 0;
-		o_Memory_Address <= 'bz;
-		o_Write_Enable <= 'bz;
+		r_Status <= 0;
+		o_Write_Status_Enable <= 0;
 		o_Indexes_Ready <= 0;
 	end else begin
 		case(r_State)
 			s_Idle: begin
-					if(i_Data_Ready == 1'b1) begin
-						r_State <= s_Request_Config_Grant;
-						o_Grant_Request <= 1;
-					end
-					else r_State <= s_Idle;
+				if(i_Status[31] == 1'b1) begin
+					r_State <= s_Read_Config;
 				end
-
-			s_Request_Config_Grant: begin
-					if (i_Grant == 1) begin
-						r_State <= s_Read_Config;
-						//prepare address for reading
-						o_Memory_Address <= 0;
-						r_Memory_Write <= 0;
-						r_Read_Counter <= 0;	
-					end else begin
-						o_Memory_Address <= 'bz;
-						// o_Write_Enable <= Z; //TODO should we?
-					end
-				end
+				else r_State <= s_Idle;
+			end
 
 			s_Read_Config: begin
-					//address has been set in previous state
-					//split config
-					o_Config <= r_Data_In;
-					r_Lambda <= r_Data_In[greek_size-1:0];
-					r_Gamma <= r_Data_In[2*greek_size-1:greek_size];
-					r_mu <= r_Data_In[3*greek_size-1:2*greek_size];
-					r_Theta <= r_Data_In[4*greek_size-1:3*greek_size]; //TODO
+				r_Lambda <= i_Config[index_width-1:0];
+				r_Gamma <= i_Config[2*index_width-1:index_width];
+				r_Theta <= i_Config[4*index_width-1:3*index_width];
 
-					//turn-off grant request
-					o_Grant_Request <= 1'b0;
-					o_Memory_Address <= 'bz;
-
-					r_State <= s_Scatter;
-					//set registers for scattering
-					o_Indexes_Ready <= 1;
-					r_row <= 0;
-					r_column <= 0;
-				end
+				r_State <= s_Scatter;
+				//set registers for scattering
+				o_Indexes_Ready <= 1;
+				r_row <= 0;
+				r_column <= 0;
+			end
 
 			s_Scatter: begin
-					//TODO what if we don't have enough blocks?
-					if (i_Indexes_Received == 1) begin
-						//generate rwo and column number for proccessors
-						/*
-						* increase row index till it goes beyound matrix width,
-						* then increase column index and make row index zero
-						*/
-						//check if proccessor received this signals
-						if (r_column + 1 >= r_Gamma) begin
-							r_column <= 0;
-							r_row <= r_row + 1;
-						end else begin
-							r_column <= r_column + 1;
-						end
-						if (r_Processor_Counter < p - 1) begin
-							o_Indexes_Ready <= o_Indexes_Ready << 1;
-							r_Processor_Counter <= r_Processor_Counter + 1;
-
-							// if (r_column + 1 >= r_Gamma) begin
-							// 	r_column <= 0;
-							// 	r_row <= r_row + 1;
-							// end else begin
-							// 	r_column <= r_column + 1;
-							// end
-						end else begin
-							r_Processor_Counter <= 0;
-							o_Indexes_Ready <= 1;
-							r_State <= s_Wait_For_Ready;
-							r_Scatter_Counter <= r_Scatter_Counter + 1;
-						end
+				//TODO what if we don't have enough blocks?
+				if (i_Indexes_Received == 1) begin
+					/*
+					 * generate rwo and column number for processors
+					 * increase row index till it goes beyound matrix width,
+					 * then increase column index and make row index zero
+					 */
+					if (r_column + 1 >= r_Gamma) begin
+						r_column <= 0;
+						r_row <= r_row + 1;
 					end else begin
-						r_State <= s_Scatter;
-					end			
-				end
+						r_column <= r_column + 1;
+					end
+					//check if we still have processor to assign
+					if (r_Processor_Counter < p - 1) begin
+						o_Indexes_Ready <= o_Indexes_Ready << 1;
+						r_Processor_Counter <= r_Processor_Counter + 1;
+					end else begin
+						r_Processor_Counter <= 0;
+						o_Indexes_Ready <= 0;
+						r_State <= s_Wait_For_Ready;
+						r_Scatter_Counter <= r_Scatter_Counter + 1;
+					end
+				end else begin
+					r_State <= s_Scatter;
+				end			
+			end
 
 			s_Wait_For_Ready: begin
-					if (i_Result_Ready == 1) begin
-						if (r_Scatter_Counter < (r_Theta - 1)) begin
-							r_State <= s_Scatter;
-						end else if (r_Scatter_Counter == (r_Theta - 1)) begin
-							r_Processor_Counter <= r_Theta * p - r_Gamma * r_Lambda;	//TODO is this synthesizable?
-							r_State <= s_Scatter;
-						end else begin
-							r_State <= s_Request_Status_Grant;
-							o_Grant_Request <= 1;
-							r_Scatter_Counter <= 0;
-						end
+				if (i_Result_Ready == 1) begin
+					if (r_Scatter_Counter < (r_Theta - 1)) begin
+						r_State <= s_Scatter;
+					end else if (r_Scatter_Counter == (r_Theta - 1)) begin
+						r_Processor_Counter <= r_Theta * p - r_Gamma * r_Lambda;	//TODO is this synthesizable?
+						r_State <= s_Scatter;
 					end else begin
-						r_State <= s_Wait_For_Ready;
-					end
-				end
-
-			s_Request_Status_Grant: begin
-					if (i_Grant == 1) begin
 						r_State <= s_Change_Status;
-						r_Status_Counter <= 0;	
-						o_Memory_Address <= 1;
-						r_Memory_Write <= 0;
-						r_Read_Counter <= 0;
-					end else begin
-						o_Memory_Address <= 'bz;
-						r_State <= s_Request_Status_Grant;
-						// o_Write_Enable <= Z; //TODO should we?
+						r_Scatter_Counter <= 0;
+						r_Status <= {i_Status[31:1], 1'b1};
+						o_Write_Status_Enable <= 1;
 					end
+				end else begin
+					r_State <= s_Wait_For_Ready;
 				end
+			end
 
 			s_Change_Status: begin
-					if (r_Status_Counter == 0) begin
-						r_State <= s_Change_Status;
-						r_Status_Counter <= 1;
-						r_Data_Out <= r_Data_In + 1;
-						r_Memory_Write <= 1;
-						o_Write_Enable <= 1;
-						r_Read_Counter <= 0;
-						// if (r_Read_Counter == 0) begin
-						// 	r_State <= s_Change_Status;
-						// 	r_Read_Counter <= 1;
-						// end else begin
-						// 	r_State <= s_Change_Status;
-						// 	r_Status_Counter <= 1;
-						// 	r_Data_Out <= {r_Data_In[31:1], 1'b1};
-						// 	r_Memory_Write <= 1;
-						// 	o_Write_Enable <= 1;
-						// 	r_Read_Counter <= 0;
-						// end
-					end else begin					
-						o_Grant_Request <= 0;
-						o_Memory_Address <= 'bz;
-						r_Read_Counter <= 0;
-						o_Write_Enable <= 'bz;
-						r_row <= 0;
-						r_column <= 0;
-						r_State <= s_Idle;
-					end
-				end
+				o_Write_Status_Enable <= 0;
+				r_State <= s_Idle;
+			end
 
 			default: r_State <= s_Idle;
 		endcase
--- Coprocessor/main_control_unit_tb.v
+++ Coprocessor/main_control_unit_tb.v
@@ -10,44 +10,30 @@ module main_control_unit_tb;
     reg reset = 1;
 
     //mocking inputs
-    reg         m_Grant;
-    reg         m_Data_Ready;
+    reg [31:0]  m_Config;
+    reg [31:0]  m_Status;
     reg         m_Indexes_Received;
     reg         m_Result_Ready;
-    reg [31:0]  m_Config_Data;
 
     //testing outputs
-    wire        t_o_Grant_Request;
-    wire [31:0] t_o_Config;
-    wire [9:0]  t_o_Memory_Address;
-    wire [31:0] t_o_Status_Data;
-    wire        t_o_Write_Enable;
+    wire [31:0] t_o_Status;
+    wire        t_o_Write_Status_Enable;
     wire [3:0]  t_o_Indexes_Ready;
     wire [7:0]  t_o_Row_Index;
     wire [7:0]  t_o_Column_Index;
-
-    //handle in-out port
-    reg r_Write_Port = 0;
-    wire [31:0] t_io_Memory_Data;
-    assign t_io_Memory_Data = (r_Write_Port) ? m_Config_Data:'bz;
-    assign t_o_Status_Data = t_io_Memory_Data;
 
     //our testing Unit
     main_CU main_control_unit (
         //inputs
         .i_Clock(clk),
         .i_Reset(reset),
-        .i_Data_Ready(m_Data_Ready),
-        .i_Grant(m_Grant),
+        .i_Config(m_Config),
+        .i_Status(m_Status),
         .i_Indexes_Received(m_Indexes_Received),
         .i_Result_Ready(m_Result_Ready),
-        //in-out port
-        .io_Memory_Data(t_io_Memory_Data),
         //outputs
-        .o_Grant_Request(t_o_Grant_Request),
-        .o_Config(t_o_Config),
-        .o_Memory_Address(t_o_Memory_Address),
-        .o_Write_Enable(t_o_Write_Enable),
+        .o_Status(t_o_Status),
+        .o_Write_Status_Enable(t_o_Write_Status_Enable),
         .o_Indexes_Ready(t_o_Indexes_Ready),
         .o_Row_Index(t_o_Row_Index),
         .o_Column_Index(t_o_Column_Index)
@@ -61,13 +47,11 @@ module main_control_unit_tb;
 
     //monitoring states
     initial begin
-        $monitor("time = %d | state = %b\n\t- Grant_Request:%b \n\t- Config_Data=%b \n\t- Memory_Addres=%b \n\t- Write_Enable=%b \n\t- IO_Memory_Data=%b \n\t- Indexes_Ready=%b \n\t- RowIndex=%d \n\t- ColumnIndex=%d",
+        $monitor("time = %d | state = %b\n\t- Config_Data=%b \n\t- Status_Data=%b \n\t- Write_Status_Enable=%b \n\t- Indexes_Ready=%b \n\t- RowIndex=%d \n\t- ColumnIndex=%d",
         $realtime, main_control_unit.r_State, 
-        t_o_Grant_Request,
-        t_o_Config,
-        t_o_Memory_Address,
-        t_o_Write_Enable,
-        t_io_Memory_Data,
+        m_Config,
+        t_o_Status,
+        t_o_Write_Status_Enable,
         t_o_Indexes_Ready,
         t_o_Row_Index,
         t_o_Column_Index
@@ -76,6 +60,7 @@ module main_control_unit_tb;
 
     //testing main_control_unit
     initial begin
+        m_Config <= 32'b00000011000000110000001100000011;
         #(0.1*half_cc)
         reset <= 0;
         #(0.8*half_cc)
@@ -84,71 +69,55 @@ module main_control_unit_tb;
         //time = 1
 
         #(0.1*half_cc)
-        m_Data_Ready <= 1;
-
+        m_Status <= 32'b10000000000000000000000000000000;
         #(1.9*half_cc)
         //time = 3
 
-        #(2.5*half_cc)
-        m_Grant <= 1;
-
-        #(1.5*half_cc)
-        //time = 7
-
-        #(0.1*half_cc)
-        r_Write_Port <= 1;
-        #(0.1*half_cc)
-        m_Config_Data <= 32'b00000011000000110000001100000011;
-        #(0.8*half_cc)
-
-        #(half_cc)
-        //we have config data - time = 9
-
         #(2*half_cc)
-        
+        //time = 5
+
         repeat (4) begin
             #(0.1*half_cc)
-            m_Indexes_Received <= 1;
-            #(2*half_cc)
             m_Indexes_Received <= 0;
+            #(2*half_cc)
+            m_Indexes_Received <= 1;
             #(1.9*half_cc);
         end
+        #(2*half_cc)
+        //time = 23
 
-        //time = 27
         #(0.1*half_cc)
         m_Result_Ready <= 1;
-            
         #(1.9*half_cc)
+
         repeat (4) begin
             #(0.1*half_cc)
-            m_Indexes_Received <= 1;
-            #(2*half_cc)
             m_Indexes_Received <= 0;
+            #(2*half_cc)
+            m_Indexes_Received <= 1;
             #(1.9*half_cc);
-            m_Result_Ready <= 0;
         end
-        //time = 45
-        // m_Result_Ready <= 0;
-        #(0.1*half_cc)
-        m_Result_Ready <= 1;
-            
-        #(1.9*half_cc)
-        //time = 47
+        m_Result_Ready <= 0;
+        #(2*half_cc)
 
         #(0.1*half_cc)
-        m_Indexes_Received <= 1;
-        #(2*half_cc)
+        m_Result_Ready <= 1;
+        #(1.9*half_cc)
+
+        #(0.1*half_cc)
         m_Indexes_Received <= 0;
-        #(1.9*half_cc);
-        //time = 51
         #(2*half_cc)
-        m_Config_Data <= 32'b10000000000000000000000000000000;
+        m_Indexes_Received <= 1;
+        #(1.9*half_cc);
+        //the end of scattering
 
-        m_Data_Ready <= 0;
-        r_Write_Port <= 1;
-        #10
+        m_Result_Ready <= 0;
+        #(2.1*half_cc)
+        m_Result_Ready <= 1;
+        #(1.9*half_cc)
 
-
+        #(4*half_cc)
+        $display("time= %d", $realtime);
         $finish;
     end
     
